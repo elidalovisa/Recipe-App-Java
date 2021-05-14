b1799da9c072b87e078ec820ab3497025c876a01
Elida Arrechea <es222vs@student.lnu.se>
2021-05-14 15:12:28 +0200

    add remove ingredient

--- Assignment 4/src/Assignment4/ListIngredients.java
+++ Assignment 4/src/Assignment4/ListIngredients.java
@@ -1,20 +1,18 @@
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class ListIngredients {
   ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
 
   // Constructor
   public ListIngredients() {
-    //ingredients = new ArrayList<Ingredient>());
+    // ingredients = new ArrayList<Ingredient>());
   }
 
   // Add ingredient and check so name is unique.
   public int addIngredient(Ingredient ingredient) {
-
-    for (int i = 0; i < this.ingredients.size(); i++) {
-      if (ingredients.get(i).name == ingredient.name) {
-        return -1;
-      }
+    if (checkIfNameExist(ingredient.name)) {
+      return -1;
     }
     ingredients.add(ingredient);
     return 0;
@@ -22,10 +20,36 @@ public class ListIngredients {
 
   public boolean checkIfNameExist(String name) {
     for (int i = 0; i < this.ingredients.size(); i++) {
-      if (ingredients.get(i).name == name) {
+      System.out.println(name);
+      if (ingredients.get(i).name.equals(name)) {
         return true;
       }
     }
     return false;
+  }
+
+  // Iterate and return all names.
+  public ArrayList<String> getAllNames() {
+    return ingredients.stream().map(s -> s.name).collect(Collectors.toCollection(ArrayList<String>::new));
+  }
+
+  // Iterate and return all ingredients.
+  public void getAllIngredients(String name) {
+    for (int i = 0; i < this.ingredients.size(); i++) {
+      if (ingredients.get(i).name.equals(name)) {
+        System.out.println(ingredients.get(i).name + " " + ingredients.get(i).unit + " " + ingredients.get(i).value
+            + " " + ingredients.get(i).price);
+      }
+    }
+  }
+
+  public void removeIngredient(String name) {
+    for (int i = 0; i < this.ingredients.size(); i++) {
+      if (ingredients.get(i).name.equals(name)) {
+      ingredients.remove(ingredients.get(i));
+        return; // return true and false
+      }
+    }
+    return;
   }
 }
--- Assignment 4/src/Assignment4/TextMenu.java
+++ Assignment 4/src/Assignment4/TextMenu.java
@@ -3,50 +3,107 @@ import java.util.Scanner;
 public class TextMenu {
 
   ListIngredients ingredients;
+  Recipe recipe;
 
   public TextMenu() {
     ingredients = new ListIngredients();
   }
 
   public void MainMenu() {
-    System.out.println("Hello welcome! Press 1 to add ingredient");
     Scanner scanner = new Scanner(System.in);
-    int choice = scanner.nextInt();
 
-    switch (choice) {
-      case 1: // To add ingredient
-        // ingredients.addIngredient();
-        addIngredientsMenu();
-        break;
+    var run = true;
+    while (run) {
+      System.out.println("Hello welcome!");
+      System.out.println("Press 1 to add ingredient");
+      System.out.println("Press 2 to list all ingredients");
+      System.out.println("Press 0 to exit program");
+      int choice = scanner.nextInt();
+      switch (choice) {
+        case 0:
+          run = false;
+          // todo save file.
+          return;
 
-      default:
-        break;
+        case 1: // To add ingredient
+          // ingredients.addIngredient();
+          addIngredientsMenu(scanner);
+          // scanner.close();
+          // MainMenu();
+          break;
+
+        case 2: // Lista ll ingredients
+          listAllIngredients(scanner);
+          break;
+
+        default:
+          break;
+      }
     }
 
     scanner.close();
   }
 
-  private void addIngredientsMenu() {
+  private void addIngredientsMenu(Scanner scanner) {
     System.out.println("What ingredient do you want to add?");
-    Scanner scanner = new Scanner(System.in);
+    scanner.nextLine();
     String name = scanner.nextLine();
     // Check if name already exist.
     while (ingredients.checkIfNameExist(name)) {
       System.out.println("Ingredient already exist, type an other one.");
       name = scanner.nextLine();
     }
-    
+
     System.out.println("What unit");
     String unit = scanner.nextLine();
     System.out.println("How much?");
     double value = scanner.nextDouble();
     System.out.println("Whats the price");
     Double price = scanner.nextDouble();
-    scanner.close();
 
     // Create a new ingredient
     Ingredient ingredient = new Ingredient(name, unit, value, price);
     ingredients.addIngredient(ingredient);
     System.out.println("Ingredient added.");
+  }
+
+  private void listAllIngredients(Scanner scanner) {
+    var allNames = ingredients.getAllNames();
+    for (int i = 0; i < allNames.size(); i++) {
+      System.out.printf("%d %s", i, allNames.get(i) + "\n");
+    }
+
+    System.out.println("Please write ingredient name to show details and/or remove ingredient.");
+    scanner.nextLine();
+    String name = scanner.nextLine();
+    ingredients.getAllIngredients(name);
+
+    System.out.println("Press 0 to remove ingredient.");
+    System.out.println("Press 1 to keep ingredient.");
+    int option = scanner.nextInt();
+    if (option == 0) {
+      ingredients.removeIngredient(name);
+      System.out.println("Ingredient removed.");
+    }
+  }
+
+  private void addRecipeMenu(Scanner scanner) {
+    System.out.println("Add recipe name");
+    scanner.nextLine();
+    String name = scanner.nextLine();
+    // Check if name already exist.
+    /*
+     * while (ingredients.checkIfNameExist(name)) {
+     * System.out.println("Ingredient already exist, type an other one."); name =
+     * scanner.nextLine(); }
+     */
+    System.out.println("How many portions?");
+    int portions = scanner.nextInt();
+
+    // Add ingredients (list)
+    // Add amount of each ingredient
+    // Add comments
+    // Add total price
+
   }
 }
