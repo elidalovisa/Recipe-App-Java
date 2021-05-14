6a34188164abdf764d4e014362d0b4b113489f60
Elida Arrechea <es222vs@student.lnu.se>
2021-05-14 10:28:25 +0200

    add class diagram

--- Assignment 4/src/App.java
+++ Assignment 4/src/App.java
@@ -1,4 +1,5 @@
 public class App {
+// Entry point for the application.
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
--- Assignment 4/src/Assignment4/Recipe.java
+++ Assignment 4/src/Assignment4/Recipe.java
@@ -1,5 +1,15 @@
 package Assignment4;
 
 public class Recipe {
+  String name;
+  String time;
   
+  Ingredient Ingredient; 
+}
+
+// Constructor method 
+public Recipe(String name, ingriedient ingrideint)
+
+public static void main(String[] args) {
+  Recipie firstRecipie = new Recipe(greekSallad, tomato);
 }
