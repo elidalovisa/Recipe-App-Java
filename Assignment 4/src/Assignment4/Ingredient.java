e3904e79d660dd4cdbe437a7043aea47c60dac8e
Elida Arrechea <es222vs@student.lnu.se>
2021-05-14 10:42:18 +0200

    add Ingredient

---
+++ Assignment 4/src/Assignment4/Ingredient.java
@@ -0,0 +1,15 @@
+public class Ingredient {
+  String name;
+  String unit;
+  Double value;
+  Double price;
+
+  // Constructor
+  public Ingredient(String name, String unit, double value, double price) {
+    this.name = name;
+    this.unit = unit;
+    this.value = value;
+    this.price = price;
+  }
+
+}
