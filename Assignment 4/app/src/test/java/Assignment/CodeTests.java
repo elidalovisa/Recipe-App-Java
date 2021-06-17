6b8a61c4daf9ee603baefd30738cf0a2e999b548
Elida Arrechea <es222vs@student.lnu.se>
2021-06-17 23:15:23 +0200

    add test to add and remove ingredients

--- Assignment 4/app/src/test/java/Assignment/CodeTests.java
+++ Assignment 4/app/src/test/java/Assignment/CodeTests.java
@@ -6,11 +6,38 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 
 public class CodeTests {
-  // Test to add one item to arrayList
+  // Test to add one ingredient to ArrayList.
   @Test 
-  public void testMethodAdd() {
-  ArrayList<String> ingredients = new ArrayList<String>();
-  ingredients.add("milk");
+  public void testMethodAddOneIngredient() {
+  ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+  Ingredient newIngredient = new Ingredient("milk", "Liter", 2, 1.5);
+  ingredients.add(newIngredient);
   assertEquals(1,ingredients.size());
   }
+
+    // Test to add one ingredient to ArrayList.
+    @Test 
+    public void testMethodAddFiveIngredients() {
+    ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+    Ingredient newIngredient = new Ingredient("milk", "Liter", 2, 1.5);
+    ingredients.add(newIngredient);
+    ingredients.add(newIngredient);
+    ingredients.add(newIngredient);
+    ingredients.add(newIngredient);
+    ingredients.add(newIngredient);
+    assertEquals(5,ingredients.size());
+    }
+
+      // Test to remove one ingredient to ArrayList.
+      @Test 
+      public void testMethodRemoveIngredient() {
+      ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+      Ingredient newIngredient = new Ingredient("milk", "Liter", 2, 1.5);
+      ingredients.add(newIngredient);
+      ingredients.add(newIngredient);
+      ingredients.add(newIngredient);
+      ingredients.add(newIngredient);
+      ingredients.remove(ingredients.get(1));
+      assertEquals(3,ingredients.size());
+      }
 }
